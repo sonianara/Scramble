063cd5eac127aac13ffb8895bfa2fb9d8c21d8bc
Jake Pickett <j.pickett67@yahoo.com>
2016-10-13 22:18:32 -0700

    Added a RandomLetters class that creates 100 new letters in a random order. Created a Letter class. Prints out a hand of 7 letters in Driver class

--- Driver.java
+++ Driver.java
@@ -2,5 +2,8 @@ public class Driver {
 
    public static void main(String [] args) {
       System.out.println("Testing ");
+      
+      RandomLetters rl = new RandomLetters();
+      for(Letter l: rl.getFullSet()) System.out.println(l.getLetter());
    }
 }
