ff21fc7b5840c42fb2187418cb2516654d58ae1a
sarahpadlipsky <Sarah.Padlipsky@gmail.com>
2016-10-20 13:28:41 -0700

    Add DictionarySearch class and words.txt

---
+++ DictionarySearch.java
@@ -0,0 +1,56 @@
+/**
+ * Represents the dictionary for the application.
+ * @author sarahpadlipsky
+ * @version October 20, 2016
+ */
+
+import java.lang.Object;
+import java.io.File;
+import java.util.Scanner;
+import java.util.HashSet;
+
+public class DictionarySearch {
+	
+	// The dictionary for the application.
+	HashSet<String> dictionary = new HashSet<String>();
+
+	public DictionarySearch(String fileName) {
+		createDictionary(fileName);
+	}
+
+   /**
+    * @param fileName The file to open
+    */
+	public void createDictionary(String fileName) {
+
+		// Makes sure file exists
+		try {
+			Scanner scanner = new Scanner(new File(fileName));
+
+			while (scanner.hasNextLine()) {
+			   Scanner scanner2 = new Scanner(scanner.nextLine());
+			   while (scanner2.hasNext()) {
+			      String s = scanner2.next();
+				   dictionary.add(s);
+			   }
+		   }
+		}
+		catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+
+   /**
+    * @return The dictionary for the application
+    */
+	public HashSet<String> getDictionary() {
+		return dictionary;
+	}
+
+   /**
+    * @return Whether words exists in dictionary
+    */
+	public boolean contains(String word) {
+		return dictionary.contains(word);
+	}
+}
--- Driver.java
+++ Driver.java
@@ -5,5 +5,14 @@ public class Driver {
       
       RandomLetters rl = new RandomLetters();
       for(Letter l: rl.getFullSet()) System.out.println(l.getLetter());
+
+      System.out.println("Testing Dictionary");
+      DictionarySearch dictionary = new DictionarySearch("words.txt");
+      //Should return true
+      System.out.println("tests: " + dictionary.contains("tests"));
+      //Should return false
+      System.out.println("thisisfalse: " + dictionary.contains("thisisfalse"));
+
+
    }
 }
